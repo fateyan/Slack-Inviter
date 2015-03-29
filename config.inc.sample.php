77b7173d2e50d190b40512cc3c949c683bc430a9
root <fateyan.tw@gmail.com>
2015-03-29 14:53:28 +0800

    add html title config.

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -6,5 +6,7 @@ $config['token']  = "";
 $config['domain'] = "";
 //---put your channel id there, it can be found on https://api.slack.com/methods/channel.list/test ---//
 $config['channels'] = array();
-
+//---The welcome message on div---//
 $config['welcomeMessage'] = "Hi ! Join our Slack";
+//---html title---//
+$config['title'] = 'Join our slack';
