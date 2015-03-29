0b382e2cd1fdedb2b990a644324cd6e27b6cd46a
root <fateyan.tw@gmail.com>
2015-03-29 14:46:30 +0800

    update configuration

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -1,6 +1,10 @@
 <?php
 $config = array();
-
+//---put your token there, it can be found on https://api.slack.com/web ---//
 $config['token']  = "";
 
 $config['domain'] = "";
+//---put your channel id there, it can be found on https://api.slack.com/methods/channel.list/test ---//
+$config['channels'] = array();
+
+$config['welcomeMessage'] = "Hi ! Join our Slack";
