5983f5e421ae729dcbb5acd25aba49bc61420730
root <fateyan.tw@gmail.com>
2015-03-29 15:24:47 +0800

    add index.php

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 require 'vendor/autoload.php';
 require 'config.inc.php';
 
+$method = empty($_GET['method']) ? 'index' : $_GET['method'];
 $object = new Fateyan\SlackInviter($config);
 
-
+if( method_exists($object, $method) ) {
+    $object->$method();
+} else {
+    $object->index();
+}
 
